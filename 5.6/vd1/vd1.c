732d163c4026ccfc7d249a7576ebb658b75d37ef
tuanvushark1 <tuanvushark@gmail.com>
2022-06-14 21:13:03 +0700

    5.6

---
+++ 5.6/vd1/vd1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a,b,c;
+	float d;
+	a = 4;
+	b = 5;
+	c = 4;
+	d = (float)(a+b+c)/3;
+	
+	printf ("lan vote 1 %d\n",a);
+	
+	printf ("lan vote 2 %d\n",b);
+	
+	printf ("lan vote 3 %d \n",c);
+	
+	printf ("%f",d);
+	return 0;
+}
